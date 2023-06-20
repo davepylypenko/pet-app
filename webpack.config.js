4bb0e53ffe7f561c161138c786e9efd71b5a9c9e
Dave <dave.pylypenko@gmail.com>
2023-06-20 14:37:22 +0300

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  devServer: {
+    historyApiFallback: true,
+    client: {
+      overlay: true,
+    },
+    hot: false,
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+    ], 
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
